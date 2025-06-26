ef7fc558f12c7ae1ec25f907f75004231f0b9b21
sofiathomas715 <sofiathomas2901@gmail.com>
2025-06-25 22:29:30 -0500

    begin basic setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../styles/fonts.css';
+export default function Navbar() {
+  return (
+    <nav>
+      <h1 className="navbartext">Home </h1>
+    </nav>
+  );
+}
